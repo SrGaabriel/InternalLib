8dfba218fd61c9205e5a41462788f85b914b0b16
xGabrielDEV <58668092+xGabrielDEV@users.noreply.github.com>
2020-03-18 11:02:43 -0300

    Updated to register the main command.

--- src/main/java/net/gabriel/internal/library/com/InternalLib.java
+++ src/main/java/net/gabriel/internal/library/com/InternalLib.java
@@ -1,10 +1,12 @@
 package net.gabriel.internal.library.com;
 
+import net.gabriel.internal.library.com.commands.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class InternalLib extends JavaPlugin {
 
     public void onEnable() {
+        getCommand("internal-lib").setExecutor(new PluginCommand());
         print("§9[InternalLib] §aThe plugin has been successfully enabled!");
     }
 
@@ -14,6 +16,10 @@ public class InternalLib extends JavaPlugin {
 
     public static InternalLib getInstance() {
         return InternalLib.getPlugin(InternalLib.class);
+    }
+
+    public void refreshConfig() {
+        // TODO I'm currently working on this.
     }
 
     private void print(String s){
